0637f980104577219d8e84a72234632b50cbfbd8
Fateskink <shaynenguyen9@gmail.com>
2024-11-03 11:00:20 +0700

    add printbin - print string to binary

--- encrypt.h
+++ encrypt.h
@@ -1,2 +1,23 @@
 /* encrypt.h */
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+
+#define kink_decrypt(x, y) kink_encrypt(x, y)
+struct s_encrypt {
+
+};
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+typedef struct s_encrypt Encrypt;
+
+int main(void);
+
+Encrypt *init_encrypt(int8, int16); // *key , size
+int8 enc_byte(void);
+int8 *kink_encrypt(int8*, int16);
--- example.c
+++ example.c
@@ -1,6 +1,48 @@
 /* example.c */
-#include <stdio.h>
+#include "encrypt.h"
+#define F fflush(stdout)
+
+void printbin(int8 *input, const int16 size) {
+  int16 i;
+  int8 *p;
+  // i = 32 aa
+  // i = 31 bb
+
+  assert(size > 0);
+
+  for (i = size, p = input; i; i--, p++) {
+    if (!((i + 1) % 2)) {
+      printf("  ");
+    }
+
+    printf("%.02x", *p);
+  }
+
+  printf("\n");
+  return;
+}
 
 int main() {
+  // Encrypt *ecr;
+  int16 skey;
+  // int16 stext;
+  char *key, *from;
+  // char *encrypted, *decrypted;
+
+  key = "kink";
+  skey = strlen(key);
+  from = "test encrypt";
+  // stext = strlen(from);
+
+  printf("init encryption...\n");
+  // ecr = init_encrypt(*key, skey);
+
+  printf("done\n");
+
+  printf("%s ->\n" , from);
+
+  // encrypted = kink_encrypt((int8 *)from, stext);
+  printbin((int8 *)key, skey);
+
   return 0;
 }
